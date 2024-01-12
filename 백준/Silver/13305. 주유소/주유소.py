0b339fb9087b439203b2b4d349e7c7ac9540e3b2
minguk0825 <138567840+minguk0825@users.noreply.github.com>
2024-01-12 13:39:50 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 46228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,14 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dis = list(map(int,input().split()))
-price = list(map(int,input().split()))
+dis = list(map(int, input().split()))
+price = list(map(int, input().split()))
 
+total = price[0] * dis[0]  # 첫 번째 도시에서 주유하고 출발
+min_price = price[0]  # 현재까지의 최소 주유소 가격 초기화
 
-p_m = 10000
-total = 0
-for i in range(n-1):
-    p_m = min(price[i],p_m)
-    total += p_m * dis[i]
-    
+for i in range(1, n-1):
+    min_price = min(min_price, price[i])  # 최소 주유소 가격 업데이트
+    total += min_price * dis[i]  # 최소 가격으로 주유하여 이동
+
 print(total)
